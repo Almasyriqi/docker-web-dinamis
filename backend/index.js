0709d148025e740577f490dc90bc45438ebcaadd
Almasyriqi <ikramalmasyriqi@gmail.com>
2021-11-22 02:14:55 +0700

    Set up environment backend

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,9 @@ import express from "express";
 import db from "./config/db.js"
 import productRoutes from "./routes/index.js";
 import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
 
@@ -16,4 +19,7 @@ app.use(cors());
 app.use(express.json());
 app.use(productRoutes);
 
-app.listen(4000, () => console.log('Server running at port 4000'));
+const PORT = process.env.NODE_DOCKER_PORT || 4000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}.`);
+});
