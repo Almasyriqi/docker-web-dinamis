04b19b0cfaf2f44aa0aea8557298a52738cac0cd
Almasyriqi <ikramalmasyriqi@gmail.com>
2021-11-23 02:50:52 +0700

    Update Product

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import ProductList from "./components/ProductList";
 import AddProduct from "./components/AddProduct";
+import EditProduct from "./components/EditProduct";
 
 function App() {
   return (
@@ -11,7 +12,9 @@ function App() {
             <Routes>
               <Route exact path="/" element={<ProductList />}>
               </Route>
-              <Route path="/add" element={<AddProduct/>}>
+              <Route path="/add" element={<AddProduct />}>
+              </Route>
+              <Route path="/edit/:id" element={<EditProduct />}>
               </Route>
             </Routes>
           </div>
