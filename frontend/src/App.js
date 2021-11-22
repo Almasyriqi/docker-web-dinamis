214740222f41102dcc69dc186cd9efae0a824bc2
Almasyriqi <ikramalmasyriqi@gmail.com>
2021-11-22 19:37:49 +0700

    welcome react

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+
+function App() {
+  return (
+    <div className="container">
+      <div class="columns">
+        <div class="column is-half is-offset-one-quarter">
+          <h2>Welcome</h2>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
