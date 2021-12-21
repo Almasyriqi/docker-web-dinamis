3dd40ea1668642a065750ca9043deeed9015b641
Almasyriqi <ikramalmasyriqi@gmail.com>
2021-12-22 00:50:02 +0700

    try again

--- frontend/src/service/api.js
+++ frontend/src/service/api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
+import env from "react-dotenv";
 
 const api = axios.create({
- baseURL: "http://152.67.108.96:4000",
+ baseURL: env.REACT_APP_API_BASE_URL,
 });
 
 export default api;
