0b3ec1acbfadf932f5179091a70056f334783e22
Almasyriqi <ikramalmasyriqi@gmail.com>
2021-12-22 23:10:27 +0700

    deploy fix

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,7 +9,7 @@ function App() {
       <div className="container">
         <div className="columns">
           <div className="column is-half is-offset-one-quarter">
-          <h1 className="title mt-5 mb-5">Web Dinamis CRUD Product</h1>
+          <h1 className="title mt-5 mb-5">Web React CRUD Product</h1>
             <Routes>
               <Route exact path="/" element={<ProductList />}>
               </Route>
