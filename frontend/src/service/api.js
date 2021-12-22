39fbda300de6cca6af9f1ff107eb63095647aeda
Almasyriqi <ikramalmasyriqi@gmail.com>
2021-12-23 03:06:04 +0700

    coba lagi

--- frontend/src/service/api.js
+++ frontend/src/service/api.js
@@ -1,8 +1,8 @@
 import axios from "axios";
-// import env from "react-dotenv";
+import env from "react-dotenv";
 
 const api = axios.create({
- baseURL: process.env.REACT_APP_API_BASE_URL,
+ baseURL: env.REACT_APP_API_BASE_URL,
 });
 
 export default api;
