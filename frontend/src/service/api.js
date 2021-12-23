d54b9ba46eb5c3dc6f7504287b11cfb583945369
Almasyriqi <ikramalmasyriqi@gmail.com>
2021-12-23 14:01:32 +0700

    deploy fix

--- frontend/src/service/api.js
+++ frontend/src/service/api.js
@@ -1,8 +1,8 @@
 import axios from "axios";
-import env from "react-dotenv";
+//import env from "react-dotenv";
 
 const api = axios.create({
- baseURL: env.REACT_APP_API_BASE_URL,
+ baseURL: "http://152.67.108.96:4000",
 });
 
 export default api;
